9543bd8475224de0b79b94c7c55780437df293e4
Alex Weiksnar <alex@zooniverse.org>
2015-08-03 16:22:25 -0500

    Delete example test

--- test/example-test.js
+++
@@ -1,8 +0,0 @@
-import {expect} from "chai"
-
-describe('The example test', function() {
-    it('works', function () {
-        var myString = "test string"
-        expect(myString).to.be.a('string');
-    });
-});
