c40dfc4286bc7745e1f9ea48a9ee7412f532b173
Alex Weiksnar <alex@zooniverse.org>
2015-08-03 11:34:58 -0500

    Add watch and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,45 @@
 var gulp = require("gulp");
 var mocha = require("gulp-mocha");
 var babel = require("babel/register");
+var browserify = require("browserify");
+var babelify = require("babelify");
+var source = require('vinyl-source-stream');
+var watchify = require("watchify");
+
+var config = {
+    entryFile: './src/index.jsx',
+    outputDir: './out/',
+    outputFile: 'bundle.js'
+};
+
+
+var bundler;
+function getBundler() {
+    if (!bundler) {
+        var options = Object.create(watchify.args);
+        options.debug = true
+        bundler = watchify(browserify(config.entryFile, options));
+    }
+    return bundler;
+};
+
+function bundle() {
+    return getBundler()
+        .transform(babelify)
+        .bundle()
+        .on('error', function(err) { console.log('Error: ' + err.message); })
+        .pipe(source(config.outputFile))
+        .pipe(gulp.dest(config.outputDir));
+}
+
+gulp.task('build', function(){
+    return bundle();
+})
+
+gulp.task('compile', ['build'] ,function(){
+    process.exit(0)
+});
+
 
 gulp.task('mocha', function() {
     return gulp.src(['t/**/*.js'])
@@ -9,4 +48,10 @@ gulp.task('mocha', function() {
                 js: babel
             }
         }));
+});
+
+gulp.task('watch', ['build'], function(){
+    getBundler().on('update', function() {
+        gulp.start('build')
+    });
 });
