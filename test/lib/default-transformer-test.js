8d50ab75e0f8b91ab05d0e3c12f4928327864ce3
Alex Weiksnar <alex@zooniverse.org>
2015-09-01 15:35:30 -0500

    Update tests for prefix change

--- test/lib/default-transformer-test.js
+++ test/lib/default-transformer-test.js
@@ -15,20 +15,20 @@ describe('default-transformer', () => {
 
     it('replaces #hashtags with hashtag links', () => {
         var tagLink = replaceSymbols('#test', {project, baseURI});
-        expect(tagLink).to.equal('<a href="#/talk/search?query=test">#test</a>');
+        expect(tagLink).to.equal('<a href="/talk/search?query=test">#test</a>');
     });
 
     it('replaces #hashtags inside of html without conflicting with urls', () => {
-        let html = `<p>#good \n https://www.zooniverse.org/#/talk/17/1403?page=1&comment=3063</p>`;
+        let html = `<p>#good \n https://www.zooniverse.org/talk/17/1403?page=1&comment=3063</p>`;
         let htmlTagLink = replaceSymbols(html, {project, baseURI});
 
-        expect(htmlTagLink).to.equal(`<p><a href="#/talk/search?query=good">#good</a> \n https://www.zooniverse.org/#/talk/17/1403?page=1&comment=3063</p>`);
+        expect(htmlTagLink).to.equal(`<p><a href="/talk/search?query=good">#good</a> \n https://www.zooniverse.org/talk/17/1403?page=1&comment=3063</p>`);
     });
 
     it('replaces ^S<subject_id> mentions with subject links', () =>{
         project = { slug: "test/project" };
         var subjectLink = replaceSymbols('^S123456', {project, baseURI});;
-        expect(subjectLink).to.equal('<a href="#/projects/test/project/talk/subjects/123456">test/project - Subject 123456</a>');
+        expect(subjectLink).to.equal('<a href="/projects/test/project/talk/subjects/123456">test/project - Subject 123456</a>');
     });
 
     it('does not format subject Ids when not in a routed context', () =>{
@@ -39,6 +39,6 @@ describe('default-transformer', () => {
     it('replaces @ownerslug/project-slug^S<subject_id> mentions with links', () => {
         var projectSubjectLink = replaceSymbols('@owner/project-d^S123456', {project, baseURI});
 
-        expect(projectSubjectLink).to.equal('<a href="#/projects/owner/project-d/talk/subjects/123456">owner/project-d - Subject 123456</a>');
+        expect(projectSubjectLink).to.equal('<a href="/projects/owner/project-d/talk/subjects/123456">owner/project-d - Subject 123456</a>');
     });
 });
