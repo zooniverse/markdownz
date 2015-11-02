1716808de320684bcc56aa080bddd30809b62d5f
Chris Snyder <bumishness@gmail.com>
2015-11-02 09:53:34 -0600

    Attempt to fix tests in Firefox/IE

--- test/components/markdown-editor-test.js
+++ test/components/markdown-editor-test.js
@@ -128,6 +128,7 @@ describe('MarkdownEditor', () => {
                  cbName = cbName || name.charAt(0).toUpperCase() + name.slice(1);
                  var cbSpy = spy.on(MarkdownEditor.prototype, `on${cbName}Click`);
                  editor = TestUtils.renderIntoDocument(<MarkdownEditor value="##blah blash" />);
+                 editor.refs = mockTextarea();
                  var button = TestUtils.findRenderedDOMComponentWithClass(editor, `talk-comment-${name}-button`);
                  TestUtils.Simulate.click(button);
                  expect(cbSpy).to.have.been.called();
